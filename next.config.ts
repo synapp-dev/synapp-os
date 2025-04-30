8afd28c89a8d25e34f6010ba2a6e294bface1c4f
Aaron J. Girton <52364128+aj0urdain@users.noreply.github.com>
2025-04-30 19:36:58 +1000

    (chore): update Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "qcpaanr39l6dixw3.public.blob.vercel-storage.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
