5652f059b10fe72044a36a9991f49c26ce357e86
aaronjgirton <agirton@intradark.com>
2025-06-16 16:24:04 +1000

    refactor: update authentication page

--- app/(auth)/auth/page.tsx
+++ app/(auth)/auth/page.tsx
@@ -1,10 +1,13 @@
 import { AuthForm } from "@/components/organisms/auth-form";
+import { Suspense } from "react";
 
 export default function AuthPage() {
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">
-        <AuthForm />
+        <Suspense fallback={<></>}>
+          <AuthForm />
+        </Suspense>
       </div>
     </div>
   );
