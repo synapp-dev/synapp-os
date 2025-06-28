c74285d2f4dc147b35dd76a9e7549a904aaf16f2
Aaron J. Girton <52364128+aj0urdain@users.noreply.github.com>
2025-06-28 10:51:22 +1000

    (feat): add organisations and projects sections to project routes

---
+++ app/(main)/[organisation]/[project]/organisations/page.tsx
@@ -0,0 +1,5 @@
+import { SettingsPageTemplate } from "@/components/templates/settings-page-template";
+
+export default function OrganisationsPage() {
+  return <SettingsPageTemplate title="Organisations" />;
+}
---
+++ app/(main)/[organisation]/[project]/projects/page.tsx
@@ -0,0 +1,5 @@
+import { SettingsPageTemplate } from "@/components/templates/settings-page-template";
+
+export default function ProjectsPage() {
+  return <SettingsPageTemplate title="Projects" />;
+}
